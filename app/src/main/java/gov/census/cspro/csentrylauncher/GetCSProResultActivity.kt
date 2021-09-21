9b7631de792f86c0f187fa639e559e516a3c207f
Oryxamus <greg@csprousers.org>
2021-09-21 16:00:39 -0400

    updated the project for CSPro 7.6

--- app/src/main/java/gov/census/cspro/csentrylauncher/GetCSProResultActivity.kt
+++ app/src/main/java/gov/census/cspro/csentrylauncher/GetCSProResultActivity.kt
@@ -11,22 +11,23 @@ import androidx.appcompat.app.AppCompatActivity
  * extras that are filled in CSPro logic.
  *
  * This is the CSPro logic that calls this activity:
- * 	SystemApp android_launcher_callback;
- *      android_launcher_callback.setArgument("KEY", ID);
- *      android_launcher_callback.setArgument("NUMERIC_VALUE", NUMERIC_VALUE);
- *      android_launcher_callback.setArgument("STRING_VALUE", strip(STRING_VALUE));
- *      android_launcher_callback.exec("gov.census.cspro.csentrylauncher", "gov.census.cspro.csentrylauncher.GetCSProResultActivity");
+ *     SystemApp android_launcher_callback;
+ *     android_launcher_callback.setArgument("KEY", ID);
+ *     android_launcher_callback.setArgument("NUMERIC_VALUE", NUMERIC_VALUE);
+ *     android_launcher_callback.setArgument("STRING_VALUE", strip(STRING_VALUE));
+ *     android_launcher_callback.exec("gov.census.cspro.csentrylauncher", "gov.census.cspro.csentrylauncher.GetCSProResultActivity");
  *
  *  The activity simply stores the data received in the intent in the database and finishes to
  *  return to CSPro.
  */
+
 class GetCSProResultActivity : AppCompatActivity() {
     private val casesViewModel: CSProCasesViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Retreive data passed from CSPro
+        // Retrieve data passed from CSPro
         val key = intent.getStringExtra("KEY")
         val sv = intent.getStringExtra("STRING_VALUE")
         val nv = intent.getDoubleExtra("NUMERIC_VALUE", 0.0)
